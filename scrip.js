f184ffc4d4dde1773f9c9127c362aea3360d80c3
dukel78 <dukel2726@gmail.com>
2024-09-27 17:01:53 -0400

    reproductor5

--- scrip.js
+++ scrip.js
@@ -1,6 +1,17 @@
-let miFuncion = (title, author) => {
-    alert(`${title} - ${author}`)
-}
+let contenedor = document.getElementById('track-list')
 
-miFuncion('affections', 'juan')
-miFuncion('yellow', 'pedrito')
+axios.get('https://api.institutoalfa.org/api/songs').then((response) => {
+    response.data.songs.map((song) => {
+        let div = document.createElement('div')
+        div.setAttribute('src', 'flex p-4 gap-2')
+
+        div.innerHTML = `
+                <img src="/imagen/musica.svg" class="rounded-full" alt="">
+                    <div>
+                        <h3 class="font-bold">${song.title}</h3>
+                        <p class="opacity-40">${song.author}</p>
+                    </div>
+        `
+        contenedor.appendChild(div)
+    })
+})
